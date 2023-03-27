e2d5c7c96e408b25d9905132ea0b8c100c106ded
K.SRAVAN KUMAR <113578253+Sravank12@users.noreply.github.com>
2023-03-27 23:01:06 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer.</h2>
-                <p className='text-gray-500 py-4 max-w-md'>Enthusiastic and hard working web developer with three years of experience seeking a position with Site Smart Solutions where I can offer my debugging expertise in Java to clients and customers.</p>
+                <p className='text-gray-500 py-4 max-w-md'>Enthusiastic and hard working web developer.</p>
             <div>
                 <Link onClick={() => setNav(!nav)} to='portfolio' spy={true} smooth={true} duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
                     Portfolio
